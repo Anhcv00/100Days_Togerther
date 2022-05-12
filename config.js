008553e67220b6f971ad9d938eeaae0e30d23e23
LeoBeoo <halimjonik@gmail.com>
2022-05-12 11:24:52 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = '';
+const giftUrl = 'https://www.facebook.com/vi.mintt';
+const eventName = '💘 Happy 100 days together 💘';
+const titleCard = 'Here’s a little gift for you. I hope you like it ';
+const contentCard = 'Although I have not met you , I knew that you were the one I wanted to be with for the rest of my life. Happy 100 days anniversary to us ,   '+ 
+'Here’s to more days, month, years with you..!! I Love You In Every Universe 💘';
+const giftImage = 'love.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
